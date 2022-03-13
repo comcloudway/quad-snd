21932ceba5a1c9dfdf35600839e68508d45ef2cc
Jakob Meier <comcloudway@ccw.icu>
2022-03-13 09:50:49 +0100

    removed mixer

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub use error::Error;
 
 #[cfg(target_os = "android")]
 #[path = "opensles_snd.rs"]
-mod snd;
+pub mod snd;
 
 #[cfg(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd"))]
 #[path = "alsa_snd.rs"]
@@ -16,34 +16,15 @@ pub mod snd;
 
 #[cfg(any(target_os = "macos", target_os = "ios"))]
 #[path = "coreaudio_snd.rs"]
-mod snd;
+pub mod snd;
 
 #[cfg(target_os = "windows")]
 #[path = "wasapi_snd.rs"]
-mod snd;
+pub mod snd;
 
 #[cfg(target_arch = "wasm32")]
 #[path = "web_snd.rs"]
-mod snd;
-
-#[cfg(not(target_arch = "wasm32"))]
-mod mixer;
-
-//pub use snd;
-
-pub struct PlaySoundParams {
-    pub looped: bool,
-    pub volume: f32,
-}
-
-impl Default for PlaySoundParams {
-    fn default() -> PlaySoundParams {
-        PlaySoundParams {
-            looped: false,
-            volume: 1.,
-        }
-    }
-}
+pub mod snd;
 
 #[derive(Copy, Clone)]
 pub struct AudioParams {
