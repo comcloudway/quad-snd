55c3641c73ec4539a7ba5d6dc1019cad22896219
Jakob Meier <comcloudway@ccw.icu>
2022-03-13 11:57:15 +0100

    attempt to get coreaudio working - error

--- src/coreaudio_snd.rs
+++ src/coreaudio_snd.rs
@@ -1,4 +1,10 @@
-use crate::PlaySoundParams;
+use crate::{
+    error::Error,
+    AudioDevice,
+    AudioDeviceImpl,
+    AudioCallback,
+    AudioParams
+};
 
 use std::sync::mpsc;
 
@@ -14,108 +20,77 @@ use coreaudio::*;
 
 mod consts {
     pub const DEVICE: &'static str = "default\0";
-    pub const RATE: u32 = 44100;
-    pub const CHANNELS: u32 = 2;
     pub const BUFFER_FRAMES: u32 = 4096;
 }
-
-pub struct AudioContext {
-    mixer_ctrl: crate::mixer::MixerControl,
-}
-
 unsafe extern "C" fn saudio_coreaudio_callback(
     user_data: *mut ::std::os::raw::c_void,
     queue: _saudio_AudioQueueRef,
     buffer: _saudio_AudioQueueBufferRef,
 ) {
-    let mut mixer = &mut *(user_data as *mut crate::mixer::Mixer);
+    let mut cb = &mut *(user_data as *mut AudioCallback);
 
     let num_frames = (*buffer).mAudioDataByteSize / (2 * 4);
     let buf =
         std::slice::from_raw_parts_mut((*buffer).mAudioData as *mut f32, num_frames as usize * 2);
 
-    mixer.fill_audio_buffer(buf, num_frames as _);
+    cb.callback(&mut buf, num_frames as usize);
 
     AudioQueueEnqueueBuffer(queue, buffer, 0, std::ptr::null_mut());
 }
 
-impl AudioContext {
-    pub fn new() -> AudioContext {
-        use crate::mixer::{self, Mixer};
+impl<CB> AudioDeviceImpl for AudioDevice<CB> where CB: AudioCallback {
+    fn resume(&mut self) -> Result<(), String> {
+        if let Some(cb) = self.callback.take() {
+            let cb = Box::new(cb);
 
-        let (mixer, mixer_ctrl) = Mixer::new();
-        let mixer = Box::new(mixer);
-
-        unsafe {
-            let fmt = _saudio_AudioStreamBasicDescription {
-                mSampleRate: consts::RATE as f64,
-                mFormatID: _saudio_kAudioFormatLinearPCM,
-                mFormatFlags: _saudio_kLinearPCMFormatFlagIsFloat
-                    | _saudio_kAudioFormatFlagIsPacked,
-                mFramesPerPacket: 1,
-                mChannelsPerFrame: consts::CHANNELS,
-                mBytesPerFrame: 4 * consts::CHANNELS,
-                mBytesPerPacket: 4 * consts::CHANNELS,
-                mBitsPerChannel: 32,
-                mReserved: 0,
-            };
-            let mut ca_audio_queue: _saudio_AudioQueueRef = std::mem::zeroed();
-            let res = AudioQueueNewOutput(
-                &fmt,
-                Some(saudio_coreaudio_callback),
-                Box::into_raw(mixer) as *mut _,
-                std::ptr::null_mut(),
-                std::ptr::null_mut(),
-                0,
-                &mut ca_audio_queue,
-            );
-            assert!(res == 0);
-            assert!(ca_audio_queue.is_null() == false);
-
-            // create 2 audio buffers
-            for _ in 0..2 {
-                let mut buf: _saudio_AudioQueueBufferRef = std::ptr::null_mut();
-                let buf_byte_size = consts::BUFFER_FRAMES * fmt.mBytesPerFrame;
-                let res = AudioQueueAllocateBuffer(ca_audio_queue, buf_byte_size, &mut buf);
-                assert!(res == 0);
-                assert!(buf.is_null() == false);
-                (*buf).mAudioDataByteSize = buf_byte_size;
-                std::ptr::write_bytes(
-                    (*buf).mAudioData as *mut u8,
+            unsafe {
+                let fmt = _saudio_AudioStreamBasicDescription {
+                    mSampleRate: self.spec.freq as f64,
+                    mFormatID: _saudio_kAudioFormatLinearPCM,
+                    mFormatFlags: _saudio_kLinearPCMFormatFlagIsFloat
+                        | _saudio_kAudioFormatFlagIsPacked,
+                    mFramesPerPacket: 1,
+                    mChannelsPerFrame: self.spec.channels as u32,
+                    mBytesPerFrame: 4 * self.spec.channels as u32,
+                    mBytesPerPacket: 4 * self.spec.channels as u32,
+                    mBitsPerChannel: 32,
+                    mReserved: 0,
+                };
+                let mut ca_audio_queue: _saudio_AudioQueueRef = std::mem::zeroed();
+                let res = AudioQueueNewOutput(
+                    &fmt,
+                    Some(saudio_coreaudio_callback),
+                    Box::into_raw(cb) as *mut _,
+                    std::ptr::null_mut(),
+                    std::ptr::null_mut(),
                     0,
-                    (*buf).mAudioDataByteSize as usize,
+                    &mut ca_audio_queue,
                 );
-                AudioQueueEnqueueBuffer(ca_audio_queue, buf, 0, std::ptr::null_mut());
+                assert!(res == 0);
+                assert!(ca_audio_queue.is_null() == false);
+
+                // create 2 audio buffers
+                for _ in 0..2 {
+                    let mut buf: _saudio_AudioQueueBufferRef = std::ptr::null_mut();
+                    let buf_byte_size = consts::BUFFER_FRAMES * fmt.mBytesPerFrame;
+                    let res = AudioQueueAllocateBuffer(ca_audio_queue, buf_byte_size, &mut buf);
+                    assert!(res == 0);
+                    assert!(buf.is_null() == false);
+                    (*buf).mAudioDataByteSize = buf_byte_size;
+                    std::ptr::write_bytes(
+                        (*buf).mAudioData as *mut u8,
+                        0,
+                        (*buf).mAudioDataByteSize as usize,
+                    );
+                    AudioQueueEnqueueBuffer(ca_audio_queue, buf, 0, std::ptr::null_mut());
+                }
+
+                let res = AudioQueueStart(ca_audio_queue, std::ptr::null_mut());
+                assert!(res == 0);
             }
-
-            let res = AudioQueueStart(ca_audio_queue, std::ptr::null_mut());
-            assert!(res == 0);
+            Ok(())
+        } else {
+            Err(String::from("No AudioCallback found"))
         }
-
-        AudioContext { mixer_ctrl }
-    }
-}
-
-pub struct Sound {
-    id: usize,
-}
-
-impl Sound {
-    pub fn load(ctx: &mut AudioContext, data: &[u8]) -> Sound {
-        let id = ctx.mixer_ctrl.load(data);
-
-        Sound { id }
-    }
-
-    pub fn play(&mut self, ctx: &mut AudioContext, params: PlaySoundParams) {
-        ctx.mixer_ctrl.play(self.id, params);
-    }
-
-    pub fn stop(&mut self, ctx: &mut AudioContext) {
-        ctx.mixer_ctrl.stop(self.id);
-    }
-
-    pub fn set_volume(&mut self, ctx: &mut AudioContext, volume: f32) {
-        ctx.mixer_ctrl.set_volume(self.id, volume);
     }
 }
