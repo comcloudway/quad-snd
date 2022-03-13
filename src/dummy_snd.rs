9a828e84e22a3831cd57fcd6cff7125962aeaacc
Jakob Meier <comcloudway@ccw.icu>
2022-03-13 09:50:58 +0100

    removed mixer

--- src/dummy_snd.rs
+++ src/dummy_snd.rs
@@ -1,33 +1,12 @@
 use crate::{
-    PlaySoundParams,
     AudioCallback,
     AudioParams,
     AudioDeviceImpl,
     AudioDevice
 };
 
-pub struct AudioContext {}
-
-impl AudioContext {
-    pub fn new() -> AudioContext {
-        AudioContext {}
-    }
-}
-
-pub struct Sound {}
-
-impl Sound {
-    pub fn load(_data: &[u8]) -> Sound {
-        Sound {}
-    }
-
-    pub fn play(&mut self, _ctx: &mut AudioContext, _params: PlaySoundParams) {}
-
-    pub fn stop(&mut self, _ctx: &mut AudioContext) {}
-
-    pub fn set_volume(&mut self, _volume: f32) {}
-}
-
 impl AudioDeviceImpl for AudioDevice {
-
+    fn resume(&self) -> Result<(), String> {
+        Ok(())
+    }
 }
